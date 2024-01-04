b7b0e8f43746c5242df19437d0a5d09b36253519
Yuri <yuri.anderson.lobo@gamail.com>
2024-01-04 17:52:56 -0300

    modularizando os arquivos

--- main.go
+++ main.go
@@ -1,43 +1,21 @@
 package main
 
 import (
-	"database/sql"
 	"html/template"
+	"loja/models"
 	"net/http"
 
 	_ "github.com/lib/pq"
 )
 
-func conectaComBancoDeDados() *sql.DB {
-	conexao := "user=postgres dbname=alura_loja password=291995 host=localhost sslmode=disable"
-	db, err := sql.Open("postgres", conexao)
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
-}
-
-type Produto struct {
-	Nome       string
-	Descricao  string
-	Preco      float64
-	Quantidade int
-}
-
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	db := conectaComBancoDeDados()
-	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camisseta", Descricao: "Azul, bem bonita.", Preco: 49, Quantidade: 2},
-		{"Tenis", "Confortável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
-		{"Produto novo", "Muito legal", 1.99, 1},
-	}
-	temp.ExecuteTemplate(w, "Index", produtos)
+	todosOsProdutos := models.BuscaTodosOsProdutos()
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+
 }
