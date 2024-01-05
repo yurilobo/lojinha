d0e3e66914a54a1979124d5d10a127e4c82a522f
Yuri <yuri.anderson.lobo@gamail.com>
2024-01-05 09:37:51 -0300

    Novos Produtos

---
+++ controllers/produtos.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"loja/models"
+	"net/http"
+	"text/template"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	todosOsProdutos := models.BuscaTodosOsProdutos()
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+
+}
--- main.go
+++ main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"html/template"
-	"loja/models"
+	"loja/routes"
 	"net/http"
 
 	_ "github.com/lib/pq"
 )
 
-var temp = template.Must(template.ParseGlob("templates/*.html"))
-
 func main() {
-	http.HandleFunc("/", index)
+	routes.CarregaRotas()
 	http.ListenAndServe(":8000", nil)
-}
-func index(w http.ResponseWriter, r *http.Request) {
-	todosOsProdutos := models.BuscaTodosOsProdutos()
-	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
-
 }
