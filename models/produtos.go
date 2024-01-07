69650b8040fbcd4786464f488158f4ac10c9a2b4
Yuri <yuri.anderson.lobo@gamail.com>
2024-01-06 21:59:09 -0300

    Finalizando projeto

--- models/produtos.go
+++ models/produtos.go
@@ -13,7 +13,7 @@ type Produto struct {
 func BuscaTodosOsProdutos() []Produto {
 	db := db.ConectaComBancoDeDados()
 
-	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos order by id asc ")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -91,4 +91,14 @@ func EditaProduto(id string) Produto {
 	}
 	defer db.Close()
 	return produtoParaAtualizar
+}
+func AtualizaProduto(id int, nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectaComBancoDeDados()
+
+	AtualizaProduto, err := db.Prepare("update produtos set nome=$1, descricao=$2, preco=$3, quantidade=$4 where id=$5")
+	if err != nil {
+		panic(err.Error())
+	}
+	AtualizaProduto.Exec(nome, descricao, preco, quantidade, id)
+	defer db.Close()
 }
